7a756ef98092159e17119e9134b274e805ad922c
Jaime Irurzun <jaime@irurzun.com>
2012-09-28 13:48:18 +0100

    Create sentry alert

--- lifeordeath/alerts.py
+++ lifeordeath/alerts.py
@@ -1,2 +1,18 @@
+from urllib import urlencode
+from tornado.httpclient import AsyncHTTPClient
+
+from settings import DEBUG
+
+
 def stdout(stamp):
     print stamp.key, stamp.timestamp
+
+
+def sentry(stamp):
+    server = 'test' if DEBUG else 'www'
+    level = 'info' if DEBUG else 'fatal'
+    message = 'lifeordeath alert: %s' % stamp.key
+    url = 'https://%s.streetlife.com/sentry-endpoint/?key=str33tl1f3s3ntry' % server
+    args = {'logger_name': 'general', 'level': level, 'message': message}
+    http = AsyncHTTPClient()
+    http.fetch(url, method='POST', body=urlencode(args), callback=None)
