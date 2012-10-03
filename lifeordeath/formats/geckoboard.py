322ecd027f322f664e239e0418364a91afb32327
Jaime Irurzun <jaime@irurzun.com>
2012-10-03 13:59:06 +0100

    Return rag_column value in minutes

--- lifeordeath/formats/geckoboard.py
+++ lifeordeath/formats/geckoboard.py
@@ -11,5 +11,5 @@ def rag_column(stamp, event):
         colour = 1
     else:
         colour = 2
-    data['item'][colour] = {'value': elapsed.seconds, 'text': stamp.key}
+    data['item'][colour] = {'value': elapsed.seconds / 60, 'text': 'mins ago'}
     return data
